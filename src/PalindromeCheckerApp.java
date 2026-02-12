53a449e8b9853c5631aa8d672d62d68ebce16905
avinavkumar0045 <avinavkumar0045@gmail.com>
2026-02-12 10:25:27 +0530

    [200~

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,7 @@
+public class PalindromeCheckerApp {
+    public static void main(String args[]){
+
+        System.out.println("Jai Shree ram , Welcome to Palindrome Checker Management app\n" +
+                "Version : 1.0"+"\nSysytem initialized successfully");
+    }
+}
